e61958076227e167dafd601aef75f6af9ebef2a2
marcosguisleri <gustavoguisleri20@gmail.com>
2026-02-16 11:46:24 -0300

    feat: adicionar algoritmo do exercício 19 para cálculo da área de terreno retangular
    Implementa programa que:
    - Lê as dimensões do terreno (comprimento e largura)
    - Calcula a área utilizando a fórmula comprimento * largura
    - Exibe as dimensões informadas e a área resultante
    - Inclui casos de teste para validação

---
+++ src/logica/exercicios1a20/Exercicio19.java
@@ -0,0 +1,14 @@
+package logica.exercicios1a20;
+
+public class Exercicio19 {
+
+    void main() {
+
+        double larguraTerreno = Double.parseDouble(IO.readln("Informe a largura do terreno: "));
+        double comprimentoTerreno = Double.parseDouble(IO.readln("Informe a comprimento do terreno: "));
+
+        IO.println("Área do Terreno = " + (larguraTerreno * comprimentoTerreno));
+
+    }
+
+}
