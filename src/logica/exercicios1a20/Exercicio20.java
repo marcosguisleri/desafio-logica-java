d6713236b3fe4fb10a5665e8805f970db77ea468
marcosguisleri <gustavoguisleri20@gmail.com>
2026-02-17 12:20:05 -0300

    feat: adiciona solução do exercício 20

---
+++ src/logica/exercicios1a20/Exercicio20.java
@@ -0,0 +1,13 @@
+package logica.exercicios1a20;
+
+public class Exercicio20 {
+
+    void main() {
+
+        int quantCavalos = Integer.parseInt(IO.readln("Informe a quantidade de cavalos: "));
+
+        IO.println("A quantidade de ferraduras necessárias para equipar todos os cavalos do haras é: " + (quantCavalos * 4));
+
+    }
+
+}
