60d1d4c7502b9b63f5bae9923430c2552d7f50ce
marcosguisleri <marcos.guisleri.dev@gmail.com>
2026-04-20 12:15:03 -0300

    feat: implementa exercício 75 com cálculo da média entre 13 e 73

---
+++ src/logica/exercicios61a80/Exercicio75.java
@@ -0,0 +1,16 @@
+package logica.exercicios61a80;
+
+public class Exercicio75 {
+
+    void main() {
+
+        double soma = 0.0;
+
+        for (int i = 14; i <= 72; i++) {
+            soma += i;
+        }
+
+        IO.println("Média dos valores lidos entre 13 e 73 é: " + soma / 59);
+
+    }
+}
