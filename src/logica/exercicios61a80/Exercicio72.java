cf7eec0d47a05117c6ae4df4112377c305afaa12
marcosguisleri <marcos.guisleri.dev@gmail.com>
2026-04-20 11:54:03 -0300

    feat: implementa exercício 72 com geração de números divisíveis por 11 com resto 5

---
+++ src/logica/exercicios61a80/Exercicio72.java
@@ -0,0 +1,14 @@
+package logica.exercicios61a80;
+
+public class Exercicio72 {
+
+    void main() {
+
+        for (int i = 1000; i <= 1999; i++) {
+            if (i % 11 == 5) {
+                IO.println("Resto == 5: " + i);
+            }
+        }
+
+    }
+}
