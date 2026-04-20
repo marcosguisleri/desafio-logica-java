0a610481f62a85293fb78dab24a76c341aa65e79
marcosguisleri <marcos.guisleri.dev@gmail.com>
2026-04-20 12:18:31 -0300

    feat: implementa exercício 76 com geração de números ímpares entre 100 e 200

---
+++ src/logica/exercicios61a80/Exercicio76.java
@@ -0,0 +1,12 @@
+package logica.exercicios61a80;
+
+public class Exercicio76 {
+
+    void main() {
+
+        for (int i = 101; i <= 200; i += 2) {
+            IO.println(i);
+        }
+
+    }
+}
